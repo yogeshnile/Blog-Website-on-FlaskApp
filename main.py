96f51eef8fed122a2c2bc6a1b0b860184d2310d3
Yogesh Nile <yogesh.work4u@gmail.com>
2020-05-27 11:17:55 +0530

    database connected

--- main.py
+++ main.py
@@ -1,17 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+from datetime import datetime
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost/blog'
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    #=================contacts table colums name============
+    #========== no, name, email, phone_no, msg, date ============
+    
+    no = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone_no = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.String(120), nullable=True)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/contact')
-def about():
-    
+@app.route('/contact', methods = ['GET','POST'])
+def contact():
+    if(request.method == 'POST'):
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+
+        #=================contacts table colums name============
+        #========== no, name, email, phone_no, msg, date ============
+
+        entry = Contacts(name=name, email=email, phone_no=phone, msg=message, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
+
+
+
     return render_template('contact.html')
 
 @app.route('/about')
-def contact():
+def about():
     
     return render_template('about.html')
 
